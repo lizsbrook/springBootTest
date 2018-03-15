85fd6be3aa3b2ebb4cb0fffc4aaa0f2a84741b82
gaoming01 <gaoming01@sanlands.com>
2018-03-15 18:39:51 +0800

    通用mapper demo测试~

--- src/main/java/com/shangde/gao/web/DaoProcessController.java
+++ src/main/java/com/shangde/gao/web/DaoProcessController.java
@@ -1,14 +1,16 @@
 package com.shangde.gao.web;
 
 import com.shangde.gao.domain.BookBean;
+import com.shangde.gao.service.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/dao")
+@RequestMapping(value = "/daoTest")
 public class DaoProcessController {
     @Autowired
     private BookBean bookBean;
@@ -16,20 +18,22 @@ public class DaoProcessController {
     @Value("${submission.url}")
     private String submissionUrl;
 
+    @Autowired
+    BookServiceImpl bookService;
+
     //测试插入数据库
-    @RequestMapping("/insertBook")
-    public String hello() {
-//        BookBean bookBean = new BookBean();
-//        bookBean.setAuthor("gaoming");
-//        bookBean.setName("xiyouji");
-//        bookBean.setPrice("100");
-//        bookServiceImpl.insert(bookBean);
-//        return bookBean.toString();
-        return "";
+    @RequestMapping(value = "/insertBook",method = RequestMethod.POST)
+    public String hello(String name,String author,String price) {
+        BookBean bookBean = new BookBean();
+        bookBean.setName(name);
+        bookBean.setAuthor(author);
+        bookBean.setPrice(price);
+        bookService.insert(bookBean);
+        return bookBean.toString();
     }
+
     @RequestMapping("/hello/{name}")
     public String hello(@PathVariable("name") String name) {
-
         return "hello: "+name+"</br>assessmentUrl: "+submissionUrl+"</br>bookBean: "+bookBean.getAuthor();
 
     }
