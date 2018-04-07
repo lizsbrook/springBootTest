610b3f1713cb844b8a49e5af669b5b65ed676d0a
gaomingjava <gaomingwudi123>
2018-04-07 18:18:53 +0800

    springboot---rabbitmq test

--- src/main/java/com/shangde/gao/Application.java
+++ src/main/java/com/shangde/gao/Application.java
@@ -2,21 +2,13 @@ package com.shangde.gao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @SpringBootApplication
 @RestController
 public class Application extends SpringBootServletInitializer {
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
