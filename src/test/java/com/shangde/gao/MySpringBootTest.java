4532ad65f49a979937a3a20812c50aa628713c0d
gaomingjava <35190526+gaomingjava@users.noreply.github.com>
2018-04-27 21:04:19 +0800

    springBoot redis一些配置~

--- src/test/java/com/shangde/gao/MySpringBootTest.java
+++ src/test/java/com/shangde/gao/MySpringBootTest.java
@@ -1,8 +1,6 @@
 package com.shangde.gao;
 
 import com.shangde.gao.dao.manager.UserManager;
-import com.shangde.gao.domain.BookBean;
-import com.shangde.gao.domain.User;
 import com.shangde.gao.service.BookServiceImpl;
 import com.shangde.gao.service.UserService;
 import org.junit.Test;
@@ -28,17 +26,17 @@ public class MySpringBootTest {
     @Test
     public void test(){
 
-        BookBean bookBean = new BookBean();
-        bookBean.setName("thinking in java111");
-        bookBean.setAuthor("gaoming111");
-        bookBean.setPrice("1001");
-        bookServiceImpl.insert(bookBean);
-
-        bookBean = new BookBean();
-        bookBean.setName("thinking in java111");
-        bookBean =bookServiceImpl.selectOne(bookBean);
-        System.out.println(bookBean);
-        System.out.println("-----测试完毕-------");
+//        BookBean bookBean = new BookBean();
+//        bookBean.setName("thinking in java111");
+//        bookBean.setAuthor("gaoming111");
+//        bookBean.setPrice("1001");
+//        bookServiceImpl.insert(bookBean);
+//
+//        bookBean = new BookBean();
+//        bookBean.setName("thinking in java111");
+//        bookBean =bookServiceImpl.selectOne(bookBean);
+//        System.out.println(bookBean);
+//        System.out.println("-----测试完毕-------");
     }
 
     @Test
@@ -52,9 +50,9 @@ public class MySpringBootTest {
     @Test
     public void insertTest()
     {
-        String openid = "obZgZ0cXxocxMg_wu5X-Vj1reOIM";
-        User selectUser = userManager.selectOne(new User(openid));
-        System.out.println("selectUser="+selectUser);
+//        String openid = "obZgZ0cXxocxMg_wu5X-Vj1reOIM";
+//        User selectUser = userManager.selectOne(new User(openid));
+//        System.out.println("selectUser="+selectUser);
 
 
     }
