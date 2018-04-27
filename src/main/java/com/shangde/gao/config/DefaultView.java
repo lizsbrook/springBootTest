5236f094ebdb392fdcd42ff359980df81c15a722
gaomingjava <35190526+gaomingjava@users.noreply.github.com>
2018-04-28 01:01:23 +0800

    springboot---相册总入口增加美化

--- src/main/java/com/shangde/gao/config/DefaultView.java
+++ src/main/java/com/shangde/gao/config/DefaultView.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class DefaultView extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers( ViewControllerRegistry registry ) {
-        registry.addViewController( "/" ).setViewName( "forward:/demo.html" );
+        registry.addViewController( "/" ).setViewName( "forward:/index.html" );
         registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
         super.addViewControllers( registry );
     }
