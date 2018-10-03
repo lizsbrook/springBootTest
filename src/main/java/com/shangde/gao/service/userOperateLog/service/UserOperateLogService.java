77f1d4405d42c529cdd81a5a3fa1347ef04f4e46
gaomingjava <gaomingwudi123>
2018-10-03 17:45:18 +0800

    springboot---增加登录记录

---
+++ src/main/java/com/shangde/gao/service/userOperateLog/service/UserOperateLogService.java
@@ -0,0 +1,14 @@
+package com.shangde.gao.service.userOperateLog.service;
+
+
+import com.shangde.gao.domain.UserOperateLog;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yf-wenhao
+ * Date: 18/9/19
+ * Time: 下午10:14
+ */
+public interface UserOperateLogService {
+    void insert(UserOperateLog userOperateLog);
+}
